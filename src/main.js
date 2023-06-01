c2ccc00efeefb45232d3a831a462abdd5205597c
Den <52295780+josserden@users.noreply.github.com>
2023-06-01 09:50:15 +0300

    Add elements into the scene

--- src/main.js
+++ src/main.js
@@ -11,16 +11,36 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 const renderer = new THREE.WebGLRenderer();
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+const pointLight = new THREE.PointLight(0xffffff, 0.3);
+const light = new THREE.DirectionalLight(0xffffff, 0.1);
+
+pointLight.position.set(1, 1, 5);
+light.position.set(0, 0, 5);
+
 const lampLoader = new OBJLoader();
 const chairLoader = new OBJLoader();
 const sofaLoader = new OBJLoader();
+
+const lampTexture = new THREE.TextureLoader().load('./src/textures/lamp.jpeg');
+const chairTexture = new THREE.TextureLoader().load(
+  './src/textures/chair.jpeg'
+);
+
+const lampMaterial = new THREE.MeshPhysicalMaterial({
+  metalness: 0.1,
+  roughness: 1,
+  map: lampTexture,
+});
+const chairMaterial = new THREE.MeshPhysicalMaterial({
+  roughness: 0.1,
+  map: chairTexture,
+});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const wallMaterial = new THREE.MeshBasicMaterial({
-  color: 0xd3d3d3,
+  color: 0x9c9ea9,
 });
 const floorMaterial = new THREE.MeshBasicMaterial({
   color: 0x808080,
@@ -48,23 +68,36 @@ camera.position.z = 5;
 scene.add(walls);
 scene.add(floor);
 scene.add(ceiling);
-scene.add(ambientLight);
+scene.add(pointLight);
+scene.add(light);
 
 lampLoader.load('./src/models/lamp.obj', lamp => {
+  lamp.traverse(child => {
+    if (child instanceof THREE.Mesh) {
+      child.material = lampMaterial;
+    }
+  });
+
   lamp.scale.set(0.0025, 0.0025, 0.0025);
 
-  lamp.rotation.y = -3.5;
+  lamp.rotation.y = -1;
 
-  lamp.position.x = 2;
-  lamp.position.y = -1;
+  lamp.position.x = 1.95;
+  lamp.position.y = -0.95;
   lamp.position.z = 2;
 
   scene.add(lamp);
 });
 
 chairLoader.load('./src/models/chair.obj', chair => {
+  chair.traverse(child => {
+    if (child instanceof THREE.Mesh) {
+      child.material = chairMaterial;
+    }
+  });
+
   chair.scale.set(0.002, 0.002, 0.002);
-  chair.rotation.y = -3;
+  chair.rotation.y = -3.15;
 
   chair.position.x = 2.25;
   chair.position.y = -1;
